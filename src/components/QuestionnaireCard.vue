b817784b1cf562023efd61bd6a0953ba61821a34
mbornard <matisse.bornard@gmail.com>
2024-09-10 09:19:31 +0200

    strucure QuesCard

---
+++ src/components/QuestionnaireCard.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {ref} from "vue";
+
+defineProps(["permission"]);
+const emit = defineEmits(["change"]);
+//const checked = ref(permission.value);
+</script>
+
+<template>
+  <div class="permission">
+    <p>{{ permission.element }}</p>
+    <input type="checkbox" id="permission.id" v-model="permission.value" @click="$emit('change', permission)">
+  </div>
+</template>
+
+<style scoped>
+.permission {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 5px;
+  width: 100%;
+  height: 100%;
+  color: black;
+}
+</style>
